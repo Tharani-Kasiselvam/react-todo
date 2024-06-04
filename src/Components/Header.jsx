d1ce6fe98188d44e9c76111ba1498d53114d4305
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-06-04 21:09:52 +0530

    Modfied Card loading via For loop

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,9 +4,11 @@ import { TodoContext } from '../App'
 import TodoList from './TodoList'
 
 const Header = ({setEditName,setEditDesc,editName,editDesc}) => {
-  const {addTodo} = useContext(TodoContext)
+  const {addTodo,todosList,updatereloadTodo,reloadTodo} = useContext(TodoContext)
+  const [todoSelStatus,settodoSelStatus] = useState("All")
 
-
+  // let reloadTodo = todosList
+  
   const loadTodo = (e) => {
     e.preventDefault()
     const name = e.target[0].value
@@ -19,6 +21,35 @@ const Header = ({setEditName,setEditDesc,editName,editDesc}) => {
     setEditDesc("")
   }
 
+   const onSelectChange = (event) => {
+    console.log("dropdown sel:",event.target.value)
+    const selectedStatus = event.target.value
+    // updateSelStatus(selectedStatus)
+    if(selectedStatus == "All"){
+      console.log("inside All todoSelStatus")
+        document.getElementById("hdrSelect").className="all"
+        const allTodos = todosList
+        updatereloadTodo(allTodos)
+    }
+    if(selectedStatus == "Completed"){
+      console.log("inside Completed todoSelStatus")
+        document.getElementById("hdrSelect").className="comp"
+        const compTodos = todosList.filter(obj => obj.status=="Completed")
+        updatereloadTodo(compTodos)
+    }
+    if(selectedStatus == "Not Completed"){
+        document.getElementById("hdrSelect").className="ncomp"
+        const ncompTodos = todosList.filter(obj => obj.status=="Not Completed")
+        updatereloadTodo(ncompTodos)
+    }    
+}
+
+    let todoArr = [];
+    for(let i=0;i<todosList.length;i++){
+      console.log(todosList[i])
+      todoArr.push(<TodoList key={todosList[i].id} todos = {todosList[i]} setEditName={setEditName} setEditDesc={setEditDesc} todoSelStatus = {todoSelStatus} />)
+    }
+
   return (
     <div>
         <div className="header">My ToDo <br /><br />
@@ -28,8 +59,27 @@ const Header = ({setEditName,setEditDesc,editName,editDesc}) => {
                 <button className="btn btn-primary" style = {{width:"100px",height:"45px",marginBottom:"7px"}}>Add Todo</button>
             </form>
         </div>
-        <TodoList setEditName={setEditName} setEditDesc={setEditDesc}/>
-    </div>
+
+        {/* Adding newly */}
+        <div className="todo-list">
+        <div className="list-head">
+            <div className="mytodo">My Todos</div>
+            <div className="status_filter">Status Filter:
+              <select id="hdrSelect" className="All" onChange={onSelectChange}>
+                <option className="all" value="All" selected>All</option> 
+                <option className="comp" value="Completed">Completed</option> 
+                <option className="ncomp" value="Not Completed">Not Completed</option> 
+              </select> 
+            </div>
+        </div>
+        {console.log("In Header Comp curr RELOAD TODO values: ",reloadTodo)}
+        {console.log("In Header Comp curr ACTUAL TODO values: ",todosList)}
+        <div className="loadtodo">
+          {todoArr}
+        </div>
+        </div>
+        </div>
+    // </div>
   )
 }
 
--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,9 +1,12 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { TodoContext } from "../App"
 import '../App.css'
 
 const TodoList = (props) => {
-    const {todosList,modifyformId,removeTodo} = useContext(TodoContext)
+    const {todosList,modifyformId,removeTodo,reloadTodo} = useContext(TodoContext)
+    // const [todoSelStatus,todoSelStatus] = useState("All")
+
+
     //onEdit button, reloading the TodoName & TodoDesc to the MyTodo Form     
     const modifyTodo = (e) => {
         e.preventDefault()
@@ -16,69 +19,98 @@ const TodoList = (props) => {
         modifyformId(editId)
     }
 
+    //onDelete button, capturing the FormID for the selected Todo which has to Deleted
     const deleteTodo = (e) => {
         console.log(e)
         console.log(e.target.id)
         removeTodo(e.target.id)
     }
 
-  return (
+    const dropdownOption = (status) => {
+        if(status=="Completed"){
+            return (
+                <select id="statusCompSel" onChange={onStatusChange}>
+                {/* <option value="green">{status}</option>
+                <option value="yellow">Not Completed</option> */}
+                <option style={{backgroundColor:"green"}} selected>{status}</option>
+                <option value="yellow">Not Completed</option>
+                </select>
+            )
+        }
+        else{
+            return (
+                <select id="statusnCompSel" onChange={onStatusChange}>
+                <option>{status}</option>
+                <option>Completed</option>
+                </select>
+            )
+        }
+    }
+
+    
+
+    const onStatusChange = (event) => {
+        const statVal = event.target.value
+        console.log(statVal)
+        if(statVal == "Completed"){
+            document.getElementById("statusnCompSel").className="comp"
+        }
+        if(statVal == "Not Completed"){
+            document.getElementById("statusnCompSel").className="ncomp"
+        }
+}
+
+// const reloadTodo = todosList.filter(obj => obj.status==todoSelStatus || todoSelStatus=="All")
+ 
+return (
     <div>
-        <div className="todo-list">
+        {console.log("In TODOLIST Comp RELOAD data: ", reloadTodo)}
+        {/* <div className="todo-list">
         <div className="list-head">
             <div className="mytodo">My Todos</div>
             <div className="status_filter">Status Filter:
-                    <div className="btn-group">
-                        <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown">
-                        All
-                        </button>
-                        <div className="dropdown-menu">
-                            <a className="dropdown-item" to="#">All</a>
-                            <a className="dropdown-item" to="#">Completed</a>
-                            <a className="dropdown-item" to="#">Not Completed</a>
-                        </div>
-                    </div>
+              <select id="hdrSelect" className="All" onChange={onSelectChange}>
+                <option className="all" value="All" selected>All</option> 
+                <option className="comp" value="Completed">Completed</option> 
+                <option className="ncomp" value="Not Completed">Not Completed</option> 
+              </select> 
             </div>
         </div>
+        {console.log("---todoSelStatus: ",todoSelStatus)} */}
 
-
-        <div className="todoCards">
-        {todosList.map(todos => 
-            
+        {/* {loadTodos()} */}
+        {/* <div className="todoCards"> */}
+        {/* {todosList.map((todos) =>  */}
+        
+        {/* Loading the Todos List based on the Status, by default it will load all the Todos list */}
+        {/* {todosList.map((todos) => */}
             <form onSubmit={modifyTodo}>
-             <div className="card" style={
-                {
-                    height:"250px",
-                    width:"350px",
-                    margin:"10px"                    
+                <div className="card" style={
+                    {
+                        height:"250px",
+                        width:"350px",
+                        margin:"10px"                    
                     }}>
-                <div className = "card-body">
-                    <label htmlFor="name"><b>Name :</b></label> <input type="text" id="todo-val-name" value={todos.name}></input> <br />
-                    {console.log("Checking Desc in CARDS:", todos.description)}
-                    <label htmlFor="desc"><b>Description:</b></label> <input type="text" id="todo-val-desc" value={todos.description}></input> <br />
-                    <b>Status:</b> 
-                        <div className="btn-group">
-                            <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown">
-                            {todos.status}
-                            </button>
-                            <div className="dropdown-menu">
-                                <a className="dropdown-item" to="#">Completed</a>
-                                <a className="dropdown-item" to="#">Not Completed</a>
-                            </div>
-                        </div> <br /><br />
-                        <div>
+                    <div className = "card-body">
+                        <label htmlFor="name"><b>Name :</b></label> 
+                        <input type="text" id="todo-val-name" value={props.todos.name} disabled></input> <br />
+                        {console.log("Checking Desc in CARDS:", props.todos.description)}
+                        <label htmlFor="desc"><b>Description:</b></label> 
+                        <input type="text" id="todo-val-desc" value={props.todos.description} disabled></input> <br /><br />
+                        <b>Status:</b> 
+                            {dropdownOption(props.todos.status,props.todos.id)}
+                            <br /><br />
                             <span className="todo-btn-modify">
-                            <button type="button" id={todos.id} className="btn btn-danger" style = {{width:"100px"}} onClick={deleteTodo}>Delete</button>
-                            <button type="submit" id={todos.id} className="btn btn-success" style = {{width:"100px"}}>Edit</button>
+                            <button type="button" id={props.todos.id} className="btn btn-danger" style = {{width:"100px"}} onClick={deleteTodo}>Delete</button>
+                            <button type="submit" id={props.todos.id} className="btn btn-success" style = {{width:"100px"}}>Edit</button>
                             </span>
-                        </div>
+                    </div>
                 </div>
-             </div>
-             </form>
-        )
-        }
-       </div>
-       </div>
+            </form>
+    {/* )} */}
+        
+       {/* </div> */}
+       {/* </div> */}
     </div>
   )
 }
